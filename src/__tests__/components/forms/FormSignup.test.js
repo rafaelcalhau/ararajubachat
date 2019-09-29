e4b2e95ed45164436ece7eb85ad121c0fcdd0c14
Rafael Calhau <rafael@calhau.me>
2019-09-29 20:07:59 -0300

    Testing the textfield  username's value through regex.

--- src/__tests__/components/forms/FormSignup.test.js
+++ src/__tests__/components/forms/FormSignup.test.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 import Button from '../../../components/material/Button'
+import InputField from '../../../components/material/InputField'
 import FormSignup from '../../../components/forms/FormSignup'
+import { regexList } from '../../../config/settings.json'
 
 describe('<FormSignup />', () => {
+  let buttonBack, buttonCreate
   let wrapper, inputFirstname, inputLastname, inputUsername, inputPassword, inputPasswordConfirm
   const changeForm = jest.fn()
 
   beforeAll(() => {
     wrapper = mount(<FormSignup changeForm={formName => changeForm(formName)} />)
 
+    buttonBack = wrapper.find(Button).at(0)
+    buttonCreate = wrapper.find(Button).at(1)
     inputFirstname = wrapper.find('input').at(0)
     inputLastname = wrapper.find('input').at(1)
     inputUsername = wrapper.find('input').at(2)
@@ -61,8 +66,8 @@ describe('<FormSignup />', () => {
   })
 
   it('should have 2 action buttons: Create and Back', () => {
-    expect(wrapper.find(Button).at(0).find('span').at(0).text()).toEqual('Create')
-    expect(wrapper.find(Button).at(1).find('span').at(0).text()).toEqual('Back')
+    expect(buttonBack.find('span').at(0).text()).toEqual('Create')
+    expect(buttonCreate.find('span').at(0).text()).toEqual('Back')
   })
 
   it('should visibility button change input password type between password and text', () => {
@@ -79,12 +84,38 @@ describe('<FormSignup />', () => {
     expect(wrapper.find('input').at(4).prop('type')).toEqual('password')
   })
 
+  it('should "Create" button start disabled', () => {
+    expect(buttonBack.props().disabled).toBeTruthy()
+  })
+
   it('should "Sign Up" button call changeForm', (done) => {
-    wrapper.find(Button).at(1).simulate('click')
+    buttonCreate.simulate('click')
 
     setTimeout(() => {
       expect(changeForm.mock.calls[0][0]).toBe('login')
       done()
     }, 500)
+  })
+
+  it('should the field username accept only the pattern [a-z0-9] starting always by a letter', async () => {
+    const regex = new RegExp(regexList.username)
+    const allowed = ['rafael', 'rafa3l']
+    const notAllowed = ['1rafael', 'Rafael', 'rafaEl']
+
+    allowed.map(value => {
+      inputUsername.simulate('change', {target: { value }})
+      expect(regex.test(inputUsername.getDOMNode().value)).toBeTruthy()
+      expect(wrapper.find(InputField).at(2).props().error).toBeFalsy()
+
+      return true
+    })
+
+    notAllowed.map(value => {
+      inputUsername.simulate('change', {target: { value }})
+      expect(regex.test(inputUsername.getDOMNode().value)).toBeFalsy()
+      expect(wrapper.find(InputField).at(2).props().error).not.toBeFalsy()
+
+      return true
+    })
   })
 })
