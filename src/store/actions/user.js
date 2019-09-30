dbe0194fde41324dfd2699a4fbd91f7d568a4cb8
Rafael Calhau <rafael@calhau.me>
2019-09-30 13:56:54 -0300

    Removed unecessary exports

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -9,11 +9,11 @@ export const verifyUsername = (apiClient, username) => (dispatch) => {
     .catch(err => dispatch(verifyUsernameFailure(err)))
 }
 
-export const verifyUsernameFailure = (err) => ({ type: 'VERIFY_USERNAME_FAILURE', data: err })
+const verifyUsernameFailure = (err) => ({ type: 'VERIFY_USERNAME_FAILURE', data: err })
 
-export const verifyUsernameRequest = () => ({ type: 'VERIFY_USERNAME_REQUEST' })
+const verifyUsernameRequest = () => ({ type: 'VERIFY_USERNAME_REQUEST' })
 
-export const verifyUsernameSuccess = (data) => ({ type: 'VERIFY_USERNAME_SUCCESS', data })
+const verifyUsernameSuccess = (data) => ({ type: 'VERIFY_USERNAME_SUCCESS', data })
 
 export default {
   verifyUsername: verifyUsername.bind(null, apiClient)
