f1d4815d91f35be04d1abafd6509d9b973829c8c
Rafael Calhau <rafael@calhau.me>
2019-09-30 14:37:17 -0300

    Integrated color property and proptypes validation for color and position

--- src/components/material/InputIcon.js
+++ src/components/material/InputIcon.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import IconButton from '@material-ui/core/IconButton'
 import InputAdornment from '@material-ui/core/InputAdornment'
 
-export default function InputIcon (props) {
+const InputIcon = (props) => {
   const Icon = props.icon
 
   return (
     <InputAdornment position={props.position || 'end'}>
-      <IconButton>
+      <IconButton style={props.color && { color: props.color }}>
         <Icon />
       </IconButton>
     </InputAdornment>
   )
 }
+
+InputIcon.propTypes = {
+  color: PropTypes.oneOf(['green', 'red']),
+  position: PropTypes.oneOf(['start', 'end'])
+}
+
+export default InputIcon
